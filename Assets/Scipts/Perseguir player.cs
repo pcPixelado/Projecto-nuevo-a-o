620f730b6d5d0a0e7c56402cfed952fdb7223947
pcPixelado <147920370+pcPixelado@users.noreply.github.com>
2025-03-15 13:24:11 +0100

    hormiga mueve mueve

--- Assets/Scipts/Perseguir player.cs
+++ Assets/Scipts/Perseguir player.cs
@@ -20,6 +20,15 @@ public class ObjectFollowerWithAvoidance : MonoBehaviour
         {
             // Establecer la posición objetivo para el NavMeshAgent
             agent.SetDestination(target.position);
+
+            // Comprobar si el agente se está moviendo
+            if (agent.velocity.sqrMagnitude > 0.1f) 
+            {
+                // Rotar hacia la dirección del movimiento
+                Vector3 direccionMovimiento = agent.velocity.normalized;
+                Quaternion nuevaRotacion = Quaternion.LookRotation(direccionMovimiento);
+                transform.rotation = Quaternion.Slerp(transform.rotation, nuevaRotacion, Time.deltaTime * 10f);
+            }
         }
     }
 }
