bacaf6657a2f79c64d60e8a902bebb085848d86c
pcPixelado <147920370+pcPixelado@users.noreply.github.com>
2025-04-26 10:31:44 +0200

    haber

--- Assets/Scipts/Player movement.cs
+++ Assets/Scipts/Player movement.cs
@@ -18,19 +18,23 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void Update()
+{
+    // SALTO
+    OnFloor = Physics.CheckSphere(floorDetector.position, 0.1f, layerFloor);
+    if (OnFloor && Input.GetButtonDown("Jump"))
     {
-        // SALTO
-        OnFloor = Physics.CheckSphere(floorDetector.position, 0.1f, layerFloor);
-        if (OnFloor && Input.GetButtonDown("Jump"))
-        {
-            rig.velocity = new Vector3(rig.velocity.x, jumpForce, rig.velocity.z);
-        }
-
-        // MOVIMIENTO
-        float horizontalMove = Input.GetAxis("Horizontal");
-        float verticalMove = Input.GetAxis("Vertical");
-
-        Vector3 direction = transform.right * horizontalMove + transform.forward * verticalMove;
-        rig.velocity = new Vector3(direction.x * speed, rig.velocity.y, direction.z * speed);
+        rig.velocity = new Vector3(rig.velocity.x, jumpForce, rig.velocity.z);
     }
+
+    // MOVIMIENTO
+    float horizontalMove = Input.GetAxis("Horizontal");
+    float verticalMove = Input.GetAxis("Vertical");
+
+    Vector3 direction = transform.right * horizontalMove + transform.forward * verticalMove;
+    
+    // Atención aquí: conservamos la velocidad Y original
+    Vector3 moveVelocity = new Vector3(direction.x * speed, rig.velocity.y, direction.z * speed);
+    rig.velocity = moveVelocity;
+}
+
 }
