280abe505526ed41ebeba26880c2379a6b34f653
pcPixelado <147920370+pcPixelado@users.noreply.github.com>
2025-05-10 12:04:53 +0200

    cosa

--- Assets/Tienda/Tienda.cs
+++ Assets/Tienda/Tienda.cs
@@ -1,15 +1,23 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Tienda : MonoBehaviour
 {
-    public GameObject lanzallamas; // objeto que ya está en el jugador
+    public GameObject lanzallamas; // Objeto que ya está en el jugador
+    public Button button1;         // Botón UI asignado desde el inspector
     private bool entregado = false;
+
+    void Start()
+    {
+        lanzallamas.SetActive(false); // Se oculta al inicio del juego
+        button1.onClick.AddListener(ComprarLanzallamas);
+    }
 
     public void ComprarLanzallamas()
     {
         if (!entregado)
         {
-            lanzallamas.SetActive(true); // ahora lo puedes usar
+            lanzallamas.SetActive(true); // Se activa al comprar
             entregado = true;
         }
     }
